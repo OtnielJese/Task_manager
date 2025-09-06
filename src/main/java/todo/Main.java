079808b06db4e8011e2b2ee2cfcfb1badf733b5a
Otniel Jese <otnieljes3@gmail.com>
2025-09-05 23:43:43 -0500

    (feat/doc) Se agrego funcionalidad y gui, add inf readme

--- src/main/java/todo/Main.java
+++ src/main/java/todo/Main.java
@@ -1,4 +1,6 @@
 package todo;
+import javax.swing.*;
+import com.formdev.flatlaf.FlatLightLaf;
 
 public class Main {
     public static void main(String[] args) {
@@ -39,6 +41,32 @@ public class Main {
         repo.deleteById(2);
         System.out.println("\nRepo - tras borrar id=2:");
         System.out.println(repo.findAll());
+
+        TaskService service = new TaskService(repo);
+
+        //service.add("Estudiar Java");
+        //service.add("Comprar pan");
+
+        System.out.println("\nTodas:");
+        System.out.println(service.listAll());
+
+        service.complete(1L);
+        System.out.println("\nCompletadas:");
+        System.out.println(service.listCompleted());
+
+        service.reopen(1L);
+        System.out.println("\nPendientes:");
+        System.out.println(service.listPending());
+
+        service.updateDescription(2L, "Comprar pan y leche");
+        System.out.println("\nTodas (descripción editada):");
+        System.out.println(service.listAll());
+
+        service.delete(1L);
+        System.out.println("\nTodas (tras borrar id=1):");
+        System.out.println(service.listAll());
+
+        SwingUtilities.invokeLater(() -> new TaskManagerFrame(service).setVisible(true));
     }
 
 }
